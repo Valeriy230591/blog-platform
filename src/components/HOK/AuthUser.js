2a00a6fed17cde6e465a35a7e022d62aaf70d71e
Valeriy <valeriybojko42@gmail.com>
2024-03-13 11:05:57 +0300

    first commit

---
+++ src/components/HOK/AuthUser.js
@@ -0,0 +1,11 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+const AuthUser = ({ user }) => {
+  if (user) {
+    return <Navigate to="/" replace />;
+  }
+
+  return <Outlet />;
+};
+
+export default AuthUser;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import configerStore from "./store/store";
+import "./index.scss";
+import App from "./components/App/App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <Provider store={configerStore}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
